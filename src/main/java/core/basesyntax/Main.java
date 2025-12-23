430b3593987fcb21576621c10f0d7d35d9ee4a4c
Vladyslav Makoda <vlad.makoda@gmail.com>
2025-12-23 22:40:40 +0100

    implemented functionality

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,17 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
         // write your code here
+        Thread tread = new MyThread();
+        Runnable runnable = new MyRunnable();
+        Thread deamonthread = new Thread(runnable);
+        deamonthread.setDaemon(true);
+
+        tread.start();
+        deamonthread.start();
     }
 }
--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -1,5 +1,12 @@
 package core.basesyntax.thread;
 
-public class MyThread {
+public class MyThread extends Thread {
     // write your code here
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 21; i++) {
+            System.out.println(i);
+        }
+    }
 }
